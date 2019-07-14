38093c8580321889045a2f5e7063aa8f232b5686
thetalent <sfalkhiller@yahoo.de>
2019-07-14 10:50:35 +0200

    Update sw_mm.js

--- sw_mm.js
+++ sw_mm.js
@@ -14,21 +14,38 @@ self.addEventListener('install',event => {
  });
  
  
- self.addEventListener('fetch',function(event){
-   if (event.request.method === 'GET' && event.request.headers.get('accept')
-       .includes('text/html')){ event.respondWith(
-   {
-     return caches.match('index2.html)
-     
-   })
-   )
-   }
-   
-   else{
-   
-  event.respondWith(fetch(event.request));
-   }
-   
- })
-    
+self.addEventListener('fetch', function(event) {
 
+    event.respondWith(
+      caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200) {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+            caches.open(cacheName)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+            return response;
+          }
+        ).catch(error => {
+          // Check if the user is offline first and is trying to navigate to a web page
+          if (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
+          // Return the offline page
+          return caches.match("index2.html");
+        }
+        });
+      })
+    );
+
+});
