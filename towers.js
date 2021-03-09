5cbf02b72026f1ca163773e9bb06ad5735916667
thetalent <sfalkhiller@yahoo.de>
2021-03-09 10:32:11 +0100

    Create towers.js

---
+++ towers.js
@@ -0,0 +1,110 @@
+state =[ [1,2,3,4,5] ,[]]
+//cs=[255,155,55]
+rh= 50
+rw=150
+gap=20
+//offset=50
+moves = 0
+colors = ["dr","dbl","r","bl","br"]
+done = false
+buttons = []
+textsize = 15
+label="REWOT"
+  
+
+
+function bpressed(b){
+    moves += 1
+    done = true
+    b = int(b)
+   
+    if (state[0].includes(b)){
+        l1 = state[0]
+        l2 = state[1]
+        turn = true
+    }
+    else {
+        l1 = state[1]
+        l2 = state[0]
+        turn = false
+    }
+    idx = l1.indexOf(b)
+    if(idx < l1.length-3){
+        alert("Too much slices to move:")
+        return
+    }
+    if((b%2) == (l2[l2.length-1]%2)){
+        alert("Not possible - same color family")
+        return
+    }
+  
+    ship = l1.slice(idx,l1.length)
+    l1 = l1.slice(0,idx)
+    l2 = l2.concat(ship)
+    if(turn){
+        state[0] = l1
+        state[1] = l2
+    }
+    else{
+       state[1] = l1
+        state[0] = l2 
+    }
+    redraw()
+
+}
+    
+
+function render(){
+ 
+ background(255,255,255,255)
+ 
+ text("Moves: " + moves,10,35)
+ buttons.forEach(function(item){item.remove()})
+
+ for(let i=0;i<2;i++){
+     let h = 300
+      
+     state[i].forEach(function(item){
+       
+         button = createButton(label[item-1]).addClass(colors[item%2])
+          button.position(gap+(i*rw)+i*gap, h,rw,rh)
+          button.size(rw,rh)
+        button.mousePressed(()=>bpressed(item))
+        buttons.push(button)
+         
+         h= h - rh
+      })
+ } 
+      
+b= createButton("reset")   
+b.position(300,400)  
+b.size(50,30) 
+b.mousePressed(()=>reset())
+b.addClass("br")
+}
+
+function reset(){
+    moves = 0
+    state =[ [1,2,3,4,5] ,[]]
+    redraw()
+}
+
+function setup(){
+    alert("Move tower from left to right. You may move up to 3 slices. No (red/blue) slice may be placed on a (red/blue) slice. Click lowest slice that you want to move.")
+    textSize(textsize);
+    
+     render()
+    noLoop()
+}
+
+
+function draw(){
+     if(JSON.stringify(state)==JSON.stringify([[],[1,2,3,4,5]])){
+         render()
+         alert("won")
+         return
+     }
+   render()
+    
+
+    }
